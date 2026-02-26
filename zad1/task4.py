fd219533821cf389063fc80edc91283cdd1e4590
radekaadek <radekaadek@gmail.com>
2026-02-26 19:11:07 +0100

    Zad1 Task4

---
+++ zad1/task4.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import linprog
+
+try:
+    data = np.loadtxt("data01.csv", delimiter=",")
+    x = data[:, 0]
+    y = data[:, 1]
+except OSError:
+    print("Nie znaleziono pliku data01.csv. Generuję przykładowe dane do testu...")
+    np.random.seed(42)
+    x = np.linspace(0, 10, 50)
+    y = 0.7 * x + 2.3 + np.random.normal(0, 0.2, 50)
+    y[5] = 120
+    y[40] = -10
+
+N = len(x)
+
+Phi = np.vstack((x, np.ones(N))).T
+
+theta_LS = np.linalg.pinv(Phi) @ y
+a_LS, b_LS = theta_LS[0], theta_LS[1]
+
+c = np.concatenate(([0, 0], np.ones(N)))
+
+I = np.eye(N)
+A_ub = np.vstack((np.hstack((Phi, -I)), np.hstack((-Phi, -I))))
+b_ub = np.concatenate((y, -y))
+
+bounds = [(None, None), (None, None)] + [(0, None)] * N
+
+res = linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds, method="highs")
+a_LP, b_LP = res.x[0], res.x[1]
+
+plt.figure(figsize=(10, 6))
+plt.scatter(x, y, color="red", marker=".", label="$(x_i, y_i)$")
+
+x_line = np.array([np.min(x), np.max(x)])
+plt.plot(
+    x_line, a_LS * x_line + b_LS, color="black", linewidth=2, label="y = ax + b (LS)"
+)
+plt.plot(
+    x_line, a_LP * x_line + b_LP, color="blue", linewidth=2, label="y = ax + b (LP)"
+)
+
+plt.ylim([np.min(y[(y > -20) & (y < 20)]) - 2, np.max(y[(y > -20) & (y < 20)]) + 2])
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend(loc="upper left")
+plt.grid(True)
+plt.title("Porównanie dopasowania LP i LS w obecności punktów odstających")
+plt.show()
